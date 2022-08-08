effd995b5a3a903b5de3ce36bc104e0752d63e12
KISHOR KUNAL <kishorkunal227@gmail.com>
2022-08-08 13:11:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/discomenergyaccounting/MeterActivity2.java
@@ -0,0 +1,28 @@
+package com.example.discomenergyaccounting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MeterActivity2 extends AppCompatActivity {
+
+    private Button meter2panel_Button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meter2);
+
+        // OnClick for Panel Activity
+        meter2panel_Button = findViewById(R.id.meter2panel_Button);
+        meter2panel_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MeterActivity2.this, PanelActivity3.class));
+            }
+        });
+    }
+}
